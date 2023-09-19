baeb0c796f99e820ad165cbd0934cb0a644ce34c
andreadote <andre.mj@yahoo.com>
2023-09-19 11:21:09 -0600

    fix: bugs Removed
    Contributing-to: https://app.slack.com/client/T05RL64LKST/C05S0NDGHD0 CLD-8-9
    
    Problem
    Solution
    Note

---
+++ .idea/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_vpc" "Andre" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    name = "AndreVpc"
+  }
+}
+resource "aws_subnet" "AndreSubnet" {
+  vpc_id                  = aws_vpc.Andre.id
+  cidr_block              = "10.0.0.0/24"  # Adjust the CIDR block for your subnet
+  availability_zone       = "us-west-2a"  # Adjust the availability zone as needed
+
+  tags = {
+    name = "AndreSubnet1"
+  }
+}
